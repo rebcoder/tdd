e900edf58c387f8ad681b33fe64a728286525200
kartik <kartik31193@gmail.com>
2024-12-04 00:11:29 +0530

    step 2

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class StringCalculator {
     public static int add(String numbers) {
-        return 0;
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        String[] numArray = numbers.split(",");
+        int sum = 0;
+        for (String num : numArray) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
     }
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -6,7 +6,10 @@ import static org.junit.Assert.*;
 public class StringCalculatorTest {
 
     @Test
-    public void testEmptyString() {
+    public void testAddMethod() {
         assertEquals(0, StringCalculator.add(""));
+        assertEquals(1, StringCalculator.add("1"));
+        assertEquals(3, StringCalculator.add("1,2"));
+        assertEquals(15, StringCalculator.add("1,2,3,4,5"));
     }
 }
